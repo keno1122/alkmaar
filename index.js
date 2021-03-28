b14efd1fa6d848243d9f8d7f28dffe8615122d1a
keno1122 <80107355+keno1122@users.noreply.github.com>
2021-03-28 12:24:41 +0200

    dsf

--- index.js
+++ index.js
@@ -38,9 +38,7 @@ client.on('message', message =>{
         client.commands.get('hallo').execute(message, args);
     }  
 
-    if(command === 'suggest'){
-        client.commands.get('suggestion').execute(message, args);
-    }  
+   
 });
  
 client.login(process.env.token);
